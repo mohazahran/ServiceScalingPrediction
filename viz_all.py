73acdc19e050273d9253efccc03e817e8cc45ea8
Jianfei Gao <gao462@purdue.edu>
2019-05-19 18:46:20 -0400

    Add result visulization.

--- viz_all.py
+++ viz_all.py
@@ -2,21 +2,43 @@ import os
 import torch
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import itertools
 
 # constants
-TITLES = {
+CASE_TITLES = {
     'mm1k' : 'M/M/1/K',
     'mmmmr': 'M/M/m/m+r',
     'lbwb' : 'Leaky Bucket',
     'cio'  : 'Circular I/O',
 }
-# // STYLES = [
-# //     {'color': 'red'   }, # // 'marker': 'o' 
-# //     {'color': 'orange'}, # // 'marker': '^' 
-# //     {'color': 'green' }, # // 'marker': 's' 
-# //     {'color': 'blue'  }, # // 'marker': '*' 
-# //     {'color': 'purple'}, # // 'marker': 'x' 
-# // ]
+
+RNG_TITLES = {
+    'l': 'Wide',
+    's': 'Narrow',
+}
+
+LOSS_TITLES = {
+    'cond': 'Conditional',
+    'resi': 'Residual',
+}
+
+MAGIC_TITLES = {
+    'rrinf': 'Russian Roulette + Infinte Split',
+    '7'    : 'Baseline-7',
+    '4'    : 'Baseline-4',
+    'rr'   : 'Russian Roulette',
+    'inf'  : 'Infinte Split',
+}
+
+STYLES = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+]
 
 def null_fix(data):
     r"""Fix loss log by early stopping
@@ -68,26 +90,28 @@ def early_stop_fix(data):
     return {'loss_lst_tr': es_tr, 'loss_lst_te': es_te}
 
 # fixing function
-_fix = null_fix
+_fix = None
 
-def add_glyph(ax, case, part, num, ctype, magic, alpha_str, label, maxlen=None, **kargs):
+def add_glyph(ax, data, case, rng, loss, num, magic, alpha, label, maxlen=None, **kargs):
     r"""Add a Glyph to given axis
 
     Args
     ----
     ax : matplotlib.axes._subplots.AxesSubplot
         Axis to add glyph.
+    data : str
+        Part of data to visualize.
     case : str
         Studying case.
-    part : str
-        Part of data to visualize.
+    rng : str
+        Lambda range used in studying case.
+    loss : str
+        Training criterion used in studying case.
     num : int
         Number of training samples used in studying case.
-    ctype : str
-        Training criterion used in studying case.
     magic : str
         Numerical method used in studying case.
-    alpha_str : str
+    alpha : str
         Alpha used in studying case in string.
     label : str
         Label of adding glyph.
@@ -103,13 +127,16 @@ def add_glyph(ax, case, part, num, ctype, magic, alpha_str, label, maxlen=None, 
 
     """
     # construct path
-    rootname = "{}_{}_{}_{}_{}".format(case, 'all', ctype, 'all', alpha_str)
-    filename = "{}_{}_{}_{}_{}.pt".format(case, num, ctype, magic, alpha_str)
+    rootname = "{}_{}_{}_{}_{}".format(case, 'all', loss, 'all', alpha)
+    filename = "{}_{}_{}_{}_{}_{}.pt".format(case, rng, num, loss, magic, alpha)
     path = os.path.join(rootname, filename)
 
     # load data
-    data = _fix(torch.load(path)) # // torch.load(path)
-    ydata = data[part]
+    data_dict = _fix(torch.load(path))
+    if data in ('tr', 'te'):
+        ydata = data_dict["loss_lst_{}".format(data)]
+    else:
+        raise NotImplementedError
     maxlen = maxlen or len(ydata)
     freq = maxlen // 10
 
@@ -119,26 +146,30 @@ def add_glyph(ax, case, part, num, ctype, magic, alpha_str, label, maxlen=None, 
     ax.plot(xdata, ydata, label=label, markevery=freq, alpha=0.85, lw=2, **kargs)
     return min(ydata), ydata[0]
 
-def best(case):
+def best(case, rng, loss):
     r"""Find best configuration for given case
 
     Args
     ----
     case : str
         Studying case.
+    rng : str
+        Studying lambda range.
+    loss : str
+        Studying loss function.
 
     """
     # search for best configuration
-    num_lst = [200] # // [1, 5, 10, 25, 50, 100, 200]
-    ctype = 'cond'
-    magic_lst = ['7', '4', 'rr', 'inf', 'rrinf']
-    alpha_str_lst = ['1'] # // ['-1', '0', '0.01', '0.1', '1', '10']
+    num_lst = [5, 10, 25, 50, 100, 200]
+    ctype = loss
+    magic_lst = ['rrinf', '7', '4', 'rr', 'inf', ]
+    alpha_str_lst = ['-1', '0', '0.01', '0.1', '1', '10', '100']
     best_point, best_cfg = None, None
     for num in num_lst:
         for magic in magic_lst:
             for alpha_str in alpha_str_lst:
                 rootname = "{}_{}_{}_{}_{}".format(case, 'all', ctype, 'all', alpha_str)
-                filename = "{}_{}_{}_{}_{}.pt".format(case, num, ctype, magic, alpha_str)
+                filename = "{}_{}_{}_{}_{}_{}.pt".format(case, rng, num, ctype, magic, alpha_str)
                 path = os.path.join(rootname, filename)
                 data = _fix(torch.load(path))
                 ydata = data['loss_lst_te']
@@ -149,163 +180,113 @@ def best(case):
                 else:
                     pass
 
+    # force to use constant configuration
+    best_cfg = (200, 'rrinf', '100')
+
     # export best configuration
-    global CASE
-    global TITLE
-    global BEST_NUM
-    global BEST_MAGIC
-    global BEST_ALPHA_STR
-    CASE, TITLE = case, TITLES[case]
-    BEST_NUM, BEST_MAGIC, BEST_ALPHA_STR = best_cfg
-    print(best_point, best_cfg)
+    print('==[ BEST ]==')
+    print("Case  : {}".format(CASE_TITLES[case]))
+    print("Range : {}".format(RNG_TITLES[rng]))
+    print("Loss  : {}".format(LOSS_TITLES[loss]))
+    print("Number: {}".format(best_cfg[0]))
+    print("Magic : {}".format(MAGIC_TITLES[best_cfg[1]]))
+    print("Alpha : {}".format(best_cfg[2]))
+    print("Result: {:.8f}".format(best_point))
+    return dict(
+        case=case, rng=rng, loss=loss, num=[best_cfg[0]], magic=[best_cfg[1]],
+        alpha=[best_cfg[2]])
 
-def magic():
-    r"""Numerical Method Comparison on M/M/1/K"""
-    title = "Numerical Method Comparison on {}".format(TITLE)
-    filename = "{}_magic".format(CASE)
-    # fig, ax = plt.subplots(1, 2, figsize=(20, 8))
+def viz(data, case, rng, loss, num, magic, alpha, num_lst=None, magic_lst=None, alpha_lst=None):
+    r"""Comparison on given configuration"""
+    # determine title
+    if num_lst is not None:
+        prefix = '#Training Observations'
+        suffix = 'num'
+        label_lst = num_lst
+    elif magic_lst is not None:
+        prefix = 'Numerical Method'
+        suffix = 'magic'
+        translate = {
+            'rrinf': 'RR-Inf',
+            '7'    : '$2^7$',
+            '4'    : '$2^4$',
+            'rr'   : 'RR',
+            'inf'  : 'Inf',
+        }
+        label_lst = [translate[itr] for itr in magic_lst]
+    elif alpha_lst is not None:
+        prefix = 'Prior Regularization Strength'
+        suffix = 'alpha'
+        label_lst = alpha_lst
+    else:
+        raise RuntimeError()
+    title = "{prefix} Comparison on {loss} {case} {rng}".format(
+        prefix=prefix, loss=LOSS_TITLES[loss], case=CASE_TITLES[case], rng=RNG_TITLES[rng])
+    filename = "{case}_{rng}_{loss}_{suffix}_{data}".format(
+        case=case, rng=rng, loss=loss, suffix=suffix, data=data)
+
+    # font
+    font = dict(fontsize=18)
+
+    # maximum length
+    maxlen = None
+
+    # allocate canvas
     fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-    ax = (None, ax)
-    # fig.suptitle(title, fontsize=TITLE_FONT)
-    # ax[0].set_title('Train', fontsize=LABEL_FONT)
-    # ax[0].set_xlabel('#Epochs', fontsize=LABEL_FONT)
-    # ax[0].set_ylabel('Observation Loss', fontsize=LABEL_FONT)
-    ax[1].set_title('Test', fontsize=LABEL_FONT)
-    ax[1].set_xlabel('#Epochs', fontsize=LABEL_FONT)
-    ax[1].set_ylabel('MSE Loss of Focusing State\nSteady Distribution', fontsize=LABEL_FONT)
-    case = CASE
-    num = BEST_NUM
-    ctype = 'cond'
-    magic = None
-    alpha_str = BEST_ALPHA_STR
-    enum_lst = ['7', '4', 'rr', 'inf', 'rrinf']
-    label_lst = [r'$2^7$', r'$2^4$', 'RR', 'Inf', 'RR-Inf']
-    maxlen = None
-    vmin_dict, vmax_dict = dict(tr=None, te=None), dict(tr=None, te=None)
-    for i, magic in enumerate(enum_lst):
-        style = {} # // STYLES[i]
-        label = label_lst[i]
-        # vmin, vmax = add_glyph(
-        #     ax[0], case, 'loss_lst_tr', num, ctype, magic, alpha_str, label=label, maxlen=maxlen, **style)
-        # vmin_dict['tr'] = vmin if vmin_dict['tr'] is None else min(vmin_dict['tr'], vmin)
-        # vmax_dict['tr'] = vmax if vmax_dict['tr'] is None else max(vmax_dict['tr'], vmax)
-        vmin, vmax = add_glyph(
-            ax[1], case, 'loss_lst_te', num, ctype, magic, alpha_str, label=label, maxlen=maxlen, **style)
-        vmin_dict['te'] = vmin if vmin_dict['te'] is None else min(vmin_dict['te'], vmin)
-        vmax_dict['te'] = vmax if vmax_dict['te'] is None else max(vmax_dict['te'], vmax)
-    # ax[0].legend(loc='upper right', fontsize=LEGEND_FONT)
-    ax[1].legend(loc='upper right', fontsize=LEGEND_FONT)
-    # offset = (vmax_dict['tr'] - vmin_dict['tr']) * 0.05
-    # ax[0].set_ylim(vmin_dict['tr'] - offset, vmax_dict['tr'] + offset)
-    offset = (vmax_dict['te'] - vmin_dict['te']) * 0.05
-    ax[1].set_ylim(vmin_dict['te'] - offset, vmax_dict['te'] + offset)
-    fig.savefig(filename)
-    plt.close(fig)
+    ax.set_xlabel('#Epochs', **font)
+    if data == 'te':
+        ax.set_ylabel('MSE Loss on Test-Focusing State', **font)
+    elif data == 'tr':
+        ax.set_ylabel("{} Loss on Train-Observed States".format(LOSS_TITLES[loss]), **font)
+    elif data == 'mu':
+        ax.set_ylabel('Training Service Rate $\hat{\mu}$', **font)
+    elif data == 'bucket':
+        ax.set_ylabel('Training Bucket Fetching Rate $\hat{\lambda_\text{B}}$', **font)
+    else:
+        raise RuntimeError()
+    ax.tick_params(axis='both', which='major', labelsize=font['fontsize'])
+    ax.tick_params(axis='both', which='minor', labelsize=font['fontsize'])
 
-def num():
-    r"""Number of Training Samples Comparison on M/M/1/K"""
-    title = "Number of Training Samples Comparison on {}".format(TITLE)
-    filename = "{}_num".format(CASE)
-    # fig, ax = plt.subplots(1, 2, figsize=(20, 8))
-    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-    ax = (None, ax)
-    # fig.suptitle(title, fontsize=TITLE_FONT)
-    # ax[0].set_title('Train', fontsize=LABEL_FONT)
-    # ax[0].set_xlabel('#Epochs', fontsize=LABEL_FONT)
-    # ax[0].set_ylabel('Observation Loss', fontsize=LABEL_FONT)
-    ax[1].set_title('Test', fontsize=LABEL_FONT)
-    ax[1].set_xlabel('#Epochs', fontsize=LABEL_FONT)
-    ax[1].set_ylabel('MSE Loss of Focusing State\nSteady Distribution', fontsize=LABEL_FONT)
-    case = CASE
-    num = None
-    ctype = 'cond'
-    magic = BEST_MAGIC
-    alpha_str = BEST_ALPHA_STR
-    enum_lst = [1, 5, 10, 25, 50, 100, 200]
-    label_lst = enum_lst
-    maxlen = None
-    vmin_dict, vmax_dict = dict(tr=None, te=None), dict(tr=None, te=None)
-    for i, num in enumerate(enum_lst):
-        style = {} # // STYLES[i]
+    # configure enumeration
+    num_lst   = num_lst or num
+    magic_lst = magic_lst or magic
+    alpha_lst = alpha_lst or alpha
+    enum_lst = itertools.product(num_lst, magic_lst, alpha_lst)
+    
+    # traverse enumeration
+    vinf, vsup = None, None
+    for i, (num, magic, alpha) in enumerate(enum_lst):
+        style = STYLES[i]
         label = label_lst[i]
-        # vmin, vmax = add_glyph(
-        #     ax[0], case, 'loss_lst_tr', num, ctype, magic, alpha_str, label=label, maxlen=maxlen, **style)
-        # vmin_dict['tr'] = vmin if vmin_dict['tr'] is None else min(vmin_dict['tr'], vmin)
-        # vmax_dict['tr'] = vmax if vmax_dict['tr'] is None else max(vmax_dict['tr'], vmax)
         vmin, vmax = add_glyph(
-            ax[1], case, 'loss_lst_te', num, ctype, magic, alpha_str, label=label, maxlen=maxlen, **style)
-        vmin_dict['te'] = vmin if vmin_dict['te'] is None else min(vmin_dict['te'], vmin)
-        vmax_dict['te'] = vmax if vmax_dict['te'] is None else max(vmax_dict['te'], vmax)
-    # ax[0].legend(loc='upper right', fontsize=LEGEND_FONT)
-    ax[1].legend(loc='upper right', fontsize=LEGEND_FONT)
-    # offset = (vmax_dict['tr'] - vmin_dict['tr']) * 0.05
-    # ax[0].set_ylim(vmin_dict['tr'] - offset, vmax_dict['tr'] + offset)
-    offset = (vmax_dict['te'] - vmin_dict['te']) * 0.05
-    ax[1].set_ylim(vmin_dict['te'] - offset, vmax_dict['te'] + offset)
-    fig.savefig(filename)
-    plt.close(fig)
+            ax, data, case, rng, loss, num, magic, alpha, label=label, maxlen=maxlen, **style)
+        vinf = vmin if vinf is None else min(vmin, vinf)
+        vsup = vmax if vsup is None else max(vmax, vsup)
+    offset = (vsup - vinf) * 0.05
+    ax.set_ylim(vinf - offset, vsup + offset)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=6, fancybox=True, **font)
 
-def alpha():
-    r"""Prior Strength comparison on M/M/1/K"""
-    title = "Prior Strength Comparison on {}".format(TITLE)
-    filename = "{}_alpha".format(CASE)
-    fig, ax = plt.subplots(1, 2, figsize=(20, 8))
-    # fig.suptitle(title, fontsize=TITLE_FONT)
-    ax[0].set_title('Train', fontsize=LABEL_FONT)
-    ax[0].set_xlabel('#Epochs', fontsize=LABEL_FONT)
-    ax[0].set_ylabel('Observation Loss', fontsize=LABEL_FONT)
-    ax[1].set_title('Test', fontsize=LABEL_FONT)
-    ax[1].set_xlabel('#Epochs', fontsize=LABEL_FONT)
-    ax[1].set_ylabel('MSE Loss of Focusing State\nSteady Distribution', fontsize=LABEL_FONT)
-    case = CASE
-    num = BEST_NUM
-    ctype = 'cond'
-    magic = BEST_MAGIC
-    alpha_str = None
-    # enum_lst = ['-1', '0', '0.01', '0.1', '1', '10']
-    # label_lst = ['Null'] + enum_lst[1:]
-    enum_lst = ['0.01', '0.1', '1', '10', '100']
-    label_lst = enum_lst
-    maxlen = None
-    vmin_dict, vmax_dict = dict(tr=None, te=None), dict(tr=None, te=None)
-    for i, alpha_str in enumerate(enum_lst):
-        style = {} # // STYLES[i]
-        label = label_lst[i]
-        vmin, vmax = add_glyph(
-            ax[0], case, 'loss_lst_tr', num, ctype, magic, alpha_str, label=label, maxlen=maxlen, **style)
-        vmin_dict['tr'] = vmin if vmin_dict['tr'] is None else min(vmin_dict['tr'], vmin)
-        vmax_dict['tr'] = vmax if vmax_dict['tr'] is None else max(vmax_dict['tr'], vmax)
-        vmin, vmax = add_glyph(
-            ax[1], case, 'loss_lst_te', num, ctype, magic, alpha_str, label=label, maxlen=maxlen, **style)
-        vmin_dict['te'] = vmin if vmin_dict['te'] is None else min(vmin_dict['te'], vmin)
-        vmax_dict['te'] = vmax if vmax_dict['te'] is None else max(vmax_dict['te'], vmax)
-    ax[0].legend(loc='upper right', fontsize=LEGEND_FONT)
-    ax[1].legend(loc='upper right', fontsize=LEGEND_FONT)
-    offset = (vmax_dict['tr'] - vmin_dict['tr']) * 0.05
-    ax[0].set_ylim(vmin_dict['tr'] - offset, vmax_dict['tr'] + offset)
-    offset = (vmax_dict['te'] - vmin_dict['te']) * 0.05
-    ax[1].set_ylim(vmin_dict['te'] - offset, vmax_dict['te'] + offset)
-    fig.savefig(filename)
+    # save visualization
+    root = 'rsfigs'
+    fig.savefig(os.path.join(root, "{}.pdf".format(filename)), format='pdf')
+    fig.savefig(os.path.join(root, "{}.png".format(filename)), format='png')
     plt.close(fig)
 
 # run all visulization
-# TITLE_FONT = 20
-LABEL_FONT = 24
-LEGEND_FONT = 20
-mpl.rcParams['xtick.labelsize'] = 12
-best('mm1k')
-magic()
-num()
-alpha()
-best('mmmmr')
-magic()
-num()
-alpha()
-best('lbwb')
-magic()
-num()
-alpha()
-best('cio')
-magic()
-num()
-alpha()
+if __name__ == '__main__':
+    _fix = null_fix
+
+    cfgs = [
+        best('mm1k' , 's', 'cond'),
+        best('mmmmr', 'l', 'cond'),
+        best('lbwb' , 'l', 'cond'),
+        best('cio'  , 'l', 'cond'),
+        best('lbwb' , 'l', 'resi'),
+        best('cio'  , 'l', 'resi')]
+
+    for cfg in cfgs:
+        viz('te', **cfg, num_lst=[5, 10, 25, 50, 100, 200])
+        viz('te', **cfg, magic_lst=['rrinf', '7', '4', 'rr', 'inf'])
+        viz('te', **cfg, alpha_lst=['0.01', '0.1', '1', '10', '100'])
+
+        viz('tr', **cfg, alpha_lst=['0.01', '0.1', '1', '10', '100'])
